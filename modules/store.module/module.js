59a840a514d10e6bce3ca2e1121e5e1b056a3ba4
Wes Marc <weslei047@gmail.com>
2026-01-08 16:20:12 -0300

    feat: Finish categories filter mobile functionality

--- modules/store.module/module.js
+++ modules/store.module/module.js
@@ -1,4 +1,26 @@
+const btnFilterWrapper = document.querySelector(".filter-btn--mobile");
+const btnFilter = document.querySelector(".filter-btn__button");
 const btnViewMore = document.querySelector(".btn__view-more-wrapper").firstElementChild;
+const params = new URLSearchParams(window.location.search);
+const hasCategory = params.has('category');
+
+console.log(btnFilterWrapper)
+
+btnFilter.addEventListener('click', function() {
+	/* Changes the button icon */
+	this.classList.toggle("filter-btn__button--active");
+	/* Animates and open the siblings li */
+	btnFilterWrapper.classList.toggle("active");
+});
+
+checkHasCategory();
+
+function checkHasCategory() {
+	if (hasCategory) {
+		btnFilter.classList.toggle("filter-btn__button--active");
+		btnFilterWrapper.classList.add("active");
+	}
+}
 
 btnViewMore.addEventListener('click', (e) => {
 	loadProductsAndPagination();
