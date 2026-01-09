e28215787377abfb2f7e5ceda646cd2fd3b4a2b8
Wes Marc <weslei047@gmail.com>
2026-01-09 17:12:16 -0300

    feat: Finish product listing and product page

--- modules/store.module/module.js
+++ modules/store.module/module.js
@@ -1,17 +1,17 @@
 const btnFilterWrapper = document.querySelector(".filter-btn--mobile");
 const btnFilter = document.querySelector(".filter-btn__button");
-const btnViewMore = document.querySelector(".btn__view-more-wrapper").firstElementChild;
+const btnViewMore = document.querySelector(".btn__view-more-wrapper")?.firstElementChild;
 const params = new URLSearchParams(window.location.search);
 const hasCategory = params.has('category');
 
-console.log(btnFilterWrapper)
-
-btnFilter.addEventListener('click', function() {
-	/* Changes the button icon */
-	this.classList.toggle("filter-btn__button--active");
-	/* Animates and open the siblings li */
-	btnFilterWrapper.classList.toggle("active");
-});
+if (btnFilter) {
+	btnFilter.addEventListener('click', function() {
+		/* Changes the button icon */
+		this.classList.toggle("filter-btn__button--active");
+		/* Animates and open the siblings li */
+		btnFilterWrapper.classList.toggle("active");
+	});
+}
 
 checkHasCategory();
 
@@ -22,10 +22,12 @@ function checkHasCategory() {
 	}
 }
 
-btnViewMore.addEventListener('click', (e) => {
-	loadProductsAndPagination();
-	e.target.parentElement.classList.add("btn__view-more-wrapper--hidden");
-});
+if (btnViewMore) {
+	btnViewMore.addEventListener('click', (e) => {
+		loadProductsAndPagination();
+		e.target.parentElement.classList.add("btn__view-more-wrapper--hidden");
+	});
+}
 
 function loadProductsAndPagination() {
 	const products = document.querySelectorAll(".store__product--hidden");
@@ -33,4 +35,4 @@ function loadProductsAndPagination() {
 	
 	products.forEach(product => product.classList.remove("store__product--hidden"));
 	pagination.classList.remove("store__pagination--hidden");
-}
+};
